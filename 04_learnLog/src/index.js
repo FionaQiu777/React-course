606fcee74a654ca32f757e3a290ffe5c3fdb27b2
PeachQIU777 <kwanchauyee@outlook.com>
2023-02-02 20:55:13 +0800

    feat: update react-scripts

---
+++ 04_learnLog/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+
+// 创建一个React元素
+const App = <div>Hello React</div>;
+
+// 获取根元素
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// 渲染元素
+root.render(App);
